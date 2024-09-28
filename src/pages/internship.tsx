58019f8743a677ded4ca525782da78b84e5c7929
Eralp127 <eralp127@outlook.com>
2024-09-28 22:18:26 +0200

    att

---
+++ src/pages/internship.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InternshipPage = () => {
+  return (
+    <div>
+      <h1>Internship Documentation</h1>
+      <p>This page contains documentation about my internship experience.</p>
+    </div>
+  );
+};
+
+export default InternshipPage;
